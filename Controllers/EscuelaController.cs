e8b18324ab9806e19790159101d5a98fdc778139
devolopez88 <89044886+devolopez88@users.noreply.github.com>
2021-10-06 16:31:16 -0400

    Start project with only Student model

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var objEscuela = new Escuela();
+            objEscuela.anio = 2022;
+            objEscuela.Id = Guid.NewGuid().ToString();
+            objEscuela.Nombre = "Jose Celestino Mutis Class";
+            //  ViewBag
+            ViewBag.info1 = "Info base";
+            return View(objEscuela);
+        }
+    }
+}
---
+++ Models/Escuela.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.Models
+{
+    public class Escuela
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public int anio { get; set; }
+    }
+}
